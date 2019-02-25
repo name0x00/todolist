11576a2964403cf98489598c184ac06d8bdf9f5d
Hao <xiezihai666@gmail.com>
2019-02-25 21:04:02 +0800

    修改了目录结构

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 window.onload = function () {
-  /*----------storage.js-----------*/
-  ;
-  (function () {
-    'use strict'
-    window.s = {};
-
-    s.set = function (key, val) {
-      var json = JSON.stringify(val);
-      localStorage.setItem(key, json);
-    }
-
-    s.get = function (key) {
-      var json = localStorage.getItem(key);
-      return JSON.parse(json);
-    }
-
-  })();
-  /*----------base.js-----------*/
   ;
   (function () {
     'use strict'
